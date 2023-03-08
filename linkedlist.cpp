f1e5bf5b63c239b621387fb4d642229b7d38d12c
Chris <cbzoozoom7@gmail.com>
2023-03-08 17:04:24 -0600

    wrote all of the LinkedList method stubs

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,31 @@
-/*
- * your comment header here
- */
+//linkedlist.cpp
+//Chris Bowman
+//Created 8 Mar 2023
+#include "linkedlist.h"
+LinkedList::LinkedList() {
+    head = nullptr;
+}
+LinkedList::~LinkedList() {
+    clearList();
+}
+bool LinkedList::addNode(int id, string *data) {
+    return false;
+}
+bool LinkedList::deleteNode(int id) {
+    return false;
+}
+bool getNode(int id, Data *out) {
+    return false;
+}
+void printList(bool backward) {
+    
+}
+int getCount() {
+    return -1;
+}
+bool clearList() {
+    return false;
+}
+bool exists(int id) {
+    return false;
+}
