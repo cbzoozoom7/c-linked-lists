5f1185fb7ff8fc48ce63d62a9ff9a5d11d4a2b06
Chris <cbzoozoom7@gmail.com>
2023-03-08 12:54:01 -0600

    wrote all of the method prototypes in linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,21 @@
-/*
- * your comment header here
- */
+//linkedlist.h
+//Chris Bowman
+//Created 8 Mar 2023
+#ifndef LINKEDLIST_H
+    #define LINKEDLIST_H
+    #include "data.h"
+    class LinkedList {
+        public:
+            LinkedList();
+            ~LinkedList();
+            bool addNode(int, string*);
+            bool deleteNode(int);
+            bool getNode(int, Data*);
+            void printList(bool = false);
+            int getCount();
+            bool clearList();
+            bool exists(int);
+        private:
+            Node *head;
+    };
+#endif
